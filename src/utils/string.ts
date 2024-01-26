0a0289ca79f070b090aa41bea66d80ac3254932f
Pascal Garber <pascal@artandcode.studio>
2024-01-26 13:08:07 +0100

    feat(slug): Add type wrapper for slugify

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,25 @@
 // deno-lint-ignore ban-ts-comment
 // @ts-ignore
-export { slug as slugify } from "https://deno.land/x/slug@v1.1.0/mod.ts";
+import { slug } from "https://deno.land/x/slug@v1.1.0/mod.ts";
+
+/**
+ * Converts a string into a valid slug
+ * @example
+ * console.log(slug("some string")); // some-string
+ * console.log(slug("some string", "_")); // some_string
+ * console.log(slug("I ♥ UNICODE")); // I-love-UNICODE
+ * console.log(slug("I ♥ UNICODE"), { lower: true }); // i-love-unicode
+ * @example
+ * slug("some ƒÚÑķÝ and ☢ string", {
+ *  replacement: "-",
+ *  remove: undefined,
+ *  lower: true,
+ *  strict: false,
+ *  locale: "vi",
+ *  trim: true,
+ *  extends: { "☢": "nuclear" },
+ * }); // some-fUNkY-and-nuclear-string
+ */
+export const slugify = (str: string, options?: string | object) => {
+  return slug(str, options);
+};
