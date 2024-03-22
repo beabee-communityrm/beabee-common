9d618f2382c8a4430dcf2a05948a2aa2555374a8
Will Franklin <will@willpf.co.uk>
2024-03-22 11:41:20 +0000

    Add PayPal payment method

--- src/data/index.ts
+++ src/data/index.ts
@@ -36,6 +36,7 @@ export enum PaymentMethod {
   StripeCard = "s_card",
   StripeSEPA = "s_sepa",
   StripeBACS = "s_bacs",
+  StripePayPal = "s_paypal",
   GoCardlessDirectDebit = "gc_direct-debit",
 }
 
