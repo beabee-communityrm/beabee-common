218955977fb8b770d21ec7f47148773f828815a2
Pascal Garber <pascal@artandcode.studio>
2024-05-10 17:39:12 +0200

    chore: Bump version to v0.21.1-alpha.0

--- src/types/get-callout-data-with.ts
+++ src/types/get-callout-data-with.ts
@@ -1,20 +1,20 @@
 import type {
   CalloutResponseViewSchema,
+  CalloutVariantData,
   GetCalloutData,
   GetCalloutFormData,
   GetCalloutWith,
   Noop,
-  CalloutVariantData,
 } from "./index.ts";
 
-export type GetCalloutDataWith<With extends GetCalloutWith> = GetCalloutData &
-  ('form' extends With ? GetCalloutFormData : Noop) &
-  ('hasAnswered' extends With ? { hasAnswered: boolean } : Noop) &
-  ('responseCount' extends With ? { responseCount: number } : Noop) &
-  ('responseViewSchema' extends With
+export type GetCalloutDataWith<With extends GetCalloutWith> =
+  & GetCalloutData
+  & ("form" extends With ? GetCalloutFormData : Noop)
+  & ("hasAnswered" extends With ? { hasAnswered: boolean } : Noop)
+  & ("responseCount" extends With ? { responseCount: number } : Noop)
+  & ("responseViewSchema" extends With
     ? { responseViewSchema: CalloutResponseViewSchema | null }
-    : Noop) &
-  ('variantNames' extends With ? { variantNames: string[] } : Noop) &
-  ('variants' extends With
-    ? { variants: Record<string, CalloutVariantData> }
+    : Noop)
+  & ("variantNames" extends With ? { variantNames: string[] } : Noop)
+  & ("variants" extends With ? { variants: Record<string, CalloutVariantData> }
     : Noop);
