d785b9a9cfc42e9b6a477e66479c20de0b18d2d2
Pascal Garber <pascal@artandcode.studio>
2024-05-10 17:36:29 +0200

    chore: Add callout variant types

--- src/types/get-callout-data-with.ts
+++ src/types/get-callout-data-with.ts
@@ -4,13 +4,17 @@ import type {
   GetCalloutFormData,
   GetCalloutWith,
   Noop,
+  CalloutVariantData,
 } from "./index.ts";
 
-export type GetCalloutDataWith<With extends GetCalloutWith> =
-  & GetCalloutData
-  & ("responseCount" extends With ? { responseCount: number } : Noop)
-  & ("hasAnswered" extends With ? { hasAnswered: boolean } : Noop)
-  & ("responseViewSchema" extends With
+export type GetCalloutDataWith<With extends GetCalloutWith> = GetCalloutData &
+  ('form' extends With ? GetCalloutFormData : Noop) &
+  ('hasAnswered' extends With ? { hasAnswered: boolean } : Noop) &
+  ('responseCount' extends With ? { responseCount: number } : Noop) &
+  ('responseViewSchema' extends With
     ? { responseViewSchema: CalloutResponseViewSchema | null }
-    : Noop)
-  & ("form" extends With ? GetCalloutFormData : Noop);
+    : Noop) &
+  ('variantNames' extends With ? { variantNames: string[] } : Noop) &
+  ('variants' extends With
+    ? { variants: Record<string, CalloutVariantData> }
+    : Noop);
